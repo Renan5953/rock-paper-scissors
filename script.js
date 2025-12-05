e705334964c62279b580141457a4b9e6daa34000
Renan5953 <172679455+Renan5953@users.noreply.github.com>
2025-12-05 19:55:46 -0300

    Add responsive UI and refactor game logic
    - Build UI using DOM methods and new CSS rules.
    - Refactor getComputerChoice() for better readability.
    - Replace the initial prompt() with option buttons.
    - Refactor playRound() to handle DOM updates.
    - Remove the match-ending loop and make the game auto-restart at 5 points.
    - Remove console.log() calls.

--- script.js
+++ script.js
@@ -1,67 +1,77 @@
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return 'rock';
-    } else if (randomNumber === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
+const buttons = document.querySelector(".choices");
+const playerName = document.querySelector(".player-name");
+const playerScore = document.querySelector(".player-score");
+const cpuScore = document.querySelector(".computer-score");
+const roundResult = document.querySelector(".round-result");
+const matchResult = document.querySelector(".match-result");
 
-function getHumanChoice() {
-    let choice = prompt("What's your choice?");
-    return choice;
+playerName.textContent = prompt("What's your name?");
+(playerName.textContent) ? null : playerName.textContent = "Player";
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
 function capitalize(word) {
     return word.at(0).toUpperCase() + word.slice(1);
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        let lowerCase = humanChoice.toLowerCase();
-
-        if ((lowerCase === 'rock' && computerChoice === 'scissors') ||
-            (lowerCase === 'paper' && computerChoice === 'rock') ||
-            (lowerCase === 'scissors' && computerChoice === 'paper')) {
-                humanScore++;
-                return `You win! ${capitalize(lowerCase)} beats ${computerChoice}.`;
-
-        } else if ((lowerCase === 'rock' && computerChoice === 'paper') ||
-                   (lowerCase === 'paper' && computerChoice === 'scissors') ||
-                   (lowerCase === 'scissors' && computerChoice === 'rock')) {
-            computerScore++;
-            return `You lose! ${capitalize(computerChoice)} beats ${lowerCase}.`;
-
-        } else {
-            return "It's a tie!";
-        }
-    }
 
     let humanSelection;
     let computerSelection;
 
-    for (let i = 0; i < 5; i++) {
-        humanSelection = getHumanChoice();
+    buttons.addEventListener('click', (e) => {
+        humanSelection = e.target;
         computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
+
+        (matchResult.textContent) ? matchResult.style.cssText = "background-color: none;" : null;
+        (matchResult.textContent) ? matchResult.textContent = "" : null;
+
+        roundResult.textContent = playRound(humanSelection.className, computerSelection);
+        playerScore.textContent = `${humanScore}`;
+        cpuScore.textContent = `${computerScore}`;
+
+        if (humanScore === 5) {
+            matchResult.textContent = "Congratulations! You're the ultimate champion!";
+            matchResult.style.cssText = "background: #008000; border: 4px solid #006400; box-shadow: 0px 0px 4px #1f1f1f;";
+            humanScore = 0;
+            computerScore = 0;
+
+        } else if (computerScore === 5) {
+            matchResult.textContent = "You've been defeated! The computer reigns supreme.";
+            matchResult.style.cssText = "background: #ff0000; border: 4px solid #8f0000; box-shadow: 0px 0px 4px #1f1f1f;";
+            humanScore = 0;
+            computerScore = 0;
+        }
+    });
+
+    function playRound(humanChoice, computerChoice) {
+
+        if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper')) {
+                humanScore++;
+                roundResult.style.cssText = "color: white; background: #008000; border: 4px solid #006400;";
+                return `You win! ${capitalize(humanChoice)} beats ${computerChoice}.`;
+
+        } else if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+                   (humanChoice === 'paper' && computerChoice === 'scissors') ||
+                   (humanChoice === 'scissors' && computerChoice === 'rock')) {
+            computerScore++;
+            roundResult.style.cssText = "color: white; background: #ff0000; border: 4px solid #8b0000;";
+            return `You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`;
+
+        } else {
+            roundResult.style.cssText = "color: white; background: #a9a9a9; border: 4px solid #808080;";
+            return "It's a tie!";
+        }
     }
-
-    let gameResult;
-
-    if (humanScore > computerScore) {
-        gameResult = "Congratulations! You're the ultimate champion!";
-    } else if (humanScore < computerScore) {
-        gameResult = "You've been defeated! The computer reigns supreme.";
-    } else {
-        gameResult = "It's a draw! A well-fought match by both sides."
-    }
-
-    return gameResult;
 }
 
-console.log(playGame());
+playGame();
